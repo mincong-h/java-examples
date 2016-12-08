9c9e2ddaf1513b269adb00f6a95d479e6d071882
Mincong HUANG <mincong.h@gmail.com>
2016-12-08 11:47:23 +0100

    test different string representations of java.lang.Class<T>

---
+++ basic/src/test/java/io/mincongh/classmethod/ClassMethodTest.java
@@ -0,0 +1,22 @@
+package io.mincongh.classmethod;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import io.mincongh.abstraction.Dog;
+
+/**
+ * Test different string representations of class {@code java.lang.Class<T>}.
+ *
+ * @author Mincong Huang
+ */
+public class ClassMethodTest {
+
+    @Test
+    public void testMethods() {
+        assertEquals("class io.mincongh.abstraction.Dog", Dog.class.toString());
+        assertEquals("io.mincongh.abstraction.Dog", Dog.class.getName());
+        assertEquals("Dog", Dog.class.getSimpleName());
+    }
+}
