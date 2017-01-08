a37244a27eea178a912a39a9e8f913425040830d
Mincong HUANG <mincong.h@gmail.com>
2017-01-08 22:09:56 +0100

    add FasterXML jackson

--- json/src/main/java/io/mincongh/json/Person.java
+++ json/src/main/java/io/mincongh/json/Person.java
@@ -8,6 +8,8 @@ public class Person {
   private String name;
   private int age;
 
+  public Person() {}
+
   public Person(String name, int age) {
     this.name = name;
     this.age = age;
---
+++ json/src/test/java/io/mincongh/json/jackson/StreamingApiTest.java
@@ -0,0 +1,46 @@
+package io.mincongh.json.jackson;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.mincongh.json.Person;
+
+/**
+ * Data Binding converts JSON to and from POJOs based either on property accessor conventions or
+ * annotations. There are two variants: simple and full data binding. Simple data binding means
+ * converting to and from Java Maps, Lists, Strings, Numbers, Booleans and nulls. Full data binding
+ * means converting to and from any Java bean type (as well as "simple" types mentioned above).
+ * org.codehaus.jackson.map.ObjectMapper performs the marshalling (writing JSON) and unmarshalling
+ * (reading JSON) for both variants. Inspired by the annotation-based (code-first) variant of JAXB.
+ *
+ * @author Mincong Huang
+ * @see http://wiki.fasterxml.com/JacksonInFiveMinutes
+ */
+public class StreamingApiTest {
+
+  private static final String JSON_OBJ = "{\"name\":\"baby\",\"age\":3}";
+
+  @Test
+  public void testFullDataBinding() throws JsonParseException, IOException {
+    JsonFactory jsonFactory = new JsonFactory();
+    JsonParser jsonParser = jsonFactory.createParser(JSON_OBJ);
+    ObjectMapper mapper = new ObjectMapper();
+
+    assertEquals(jsonParser.nextToken(), JsonToken.START_OBJECT);
+
+    Person person = mapper.readValue(jsonParser, Person.class);
+    assertEquals("baby", person.getName());
+    assertEquals(3, person.getAge());
+  }
+
+  // TODO test simple data binding
+}
