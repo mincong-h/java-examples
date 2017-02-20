a5d083917e302e6ea5054a28ac5d6c2b7ffba5d9
Mincong HUANG <mhuang@nuxeo.com>
2017-02-20 11:32:43 +0100

    fix Travis CI failure of build 75
    https://travis-ci.org/mincong-h/java-examples/builds/202432332

--- date/src/test/java/io/mincongh/date/GregorianCalendarTest.java
+++ date/src/test/java/io/mincongh/date/GregorianCalendarTest.java
@@ -17,10 +17,12 @@ public class GregorianCalendarTest {
   @Test
   public void testConvertGregorianCalendarToStringIso8601() {
     GregorianCalendar calendar = new GregorianCalendar(2017, Calendar.FEBRUARY, 16, 20, 22, 28);
-    Date date = calendar.getTime();
+    calendar.setTimeZone(TimeZone.getTimeZone("CET"));
 
     SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
     sdf.setTimeZone(TimeZone.getTimeZone("CET"));
+
+    Date date = calendar.getTime();
     assertEquals("2017-02-16T20:22:28.000+01:00", sdf.format(date));
   }
 
