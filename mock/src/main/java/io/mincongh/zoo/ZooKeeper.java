0bdd911feb032c3d44153cc8b204165cd1a75875
Mincong HUANG <mincong.h@gmail.com>
2017-11-11 11:09:26 +0100

    Format file

--- mock/src/main/java/io/mincongh/zoo/ZooKeeper.java
+++ mock/src/main/java/io/mincongh/zoo/ZooKeeper.java
@@ -2,7 +2,6 @@ package io.mincongh.zoo;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.hibernate.Criteria;
 import org.hibernate.ScrollMode;
 import org.hibernate.ScrollableResults;
@@ -16,43 +15,43 @@ import org.hibernate.criterion.Projections;
  */
 public class ZooKeeper {
 
-    public static int doubleCountAnimal(Session session) {
-        Criteria criteria = session.createCriteria(Animal.class);
-        criteria = criteria.setProjection(Projections.rowCount());
-        long rowCount = (long) criteria.uniqueResult();
-        return (int) rowCount * 2;
-    }
+  public static int doubleCountAnimal(Session session) {
+    Criteria criteria = session.createCriteria(Animal.class);
+    criteria = criteria.setProjection(Projections.rowCount());
+    long rowCount = (long) criteria.uniqueResult();
+    return (int) rowCount * 2;
+  }
 
-    public static List<Animal> scrollAnimals(SessionFactory sf) {
-        List<Animal> animals = new ArrayList<>();
-        StatelessSession ss = null;
-        ScrollableResults scroll = null;
-        try {
-            ss = sf.openStatelessSession();
-            scroll = ss.createCriteria(Animal.class)
-                    .setReadOnly(true)
-                    .setFetchSize(1000)
-                    .scroll(ScrollMode.FORWARD_ONLY);
-            int i = 0;
-            while (scroll.next()) {
-                System.out.println("Getting the next animal ... " + i);
-                Animal a = (Animal) scroll.get(0);
-                animals.add(a);
-                System.out.println(a.getName());
-                i++;
-            }
-        } finally {
-            try {
-                scroll.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            try {
-                ss.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        return animals;
+  public static List<Animal> scrollAnimals(SessionFactory sf) {
+    List<Animal> animals = new ArrayList<>();
+    StatelessSession ss = null;
+    ScrollableResults scroll = null;
+    try {
+      ss = sf.openStatelessSession();
+      scroll = ss.createCriteria(Animal.class)
+          .setReadOnly(true)
+          .setFetchSize(1000)
+          .scroll(ScrollMode.FORWARD_ONLY);
+      int i = 0;
+      while (scroll.next()) {
+        System.out.println("Getting the next animal ... " + i);
+        Animal a = (Animal) scroll.get(0);
+        animals.add(a);
+        System.out.println(a.getName());
+        i++;
+      }
+    } finally {
+      try {
+        scroll.close();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+      try {
+        ss.close();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
     }
+    return animals;
+  }
 }
