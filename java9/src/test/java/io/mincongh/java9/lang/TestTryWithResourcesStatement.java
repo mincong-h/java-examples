10bb1862f4403957b56fc18a9b94319a0bcdc4c7
Mincong HUANG <mincong.h@gmail.com>
2018-02-28 21:03:12 +0100

    JLS 9: 14.20.3.1. Basic try-with-resources

---
+++ java9/src/test/java/io/mincongh/java9/lang/TestTryWithResourcesStatement.java
@@ -0,0 +1,49 @@
+package io.mincongh.java9.lang;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Tests try-with-resources statement. Since Java 9, the variable declaration before each managed
+ * resource is no longer required. The old syntax:
+ *
+ * <pre>
+ * try ({VariableModifier} R Identifier = Expression ...)
+ *     Block
+ * </pre>
+ *
+ * can be now simplified to:
+ *
+ * <pre>
+ * try (VariableAccess ...)
+ *     Block
+ * </pre>
+ *
+ * @author Mincong Huang
+ * @see <a
+ *     href="https://docs.oracle.com/javase/specs/jls/se8/html/jls-14.html#jls-14.20.3.1">14.20.3.1.
+ *     Basic try-with-resources</a>
+ */
+public class TestTryWithResourcesStatement {
+
+  @Test
+  public void autoCloseableSyntax() {
+    Foo foo = new Foo();
+    try (foo) {
+      assertFalse(foo.isClosed);
+    }
+    assertTrue(foo.isClosed);
+  }
+
+  private class Foo implements AutoCloseable {
+
+    private boolean isClosed = false;
+
+    @Override
+    public void close() {
+      isClosed = true;
+    }
+  }
+}
