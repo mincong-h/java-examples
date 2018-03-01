0c26765c304736a614a7bf8774de6498e26f63a0
Mincong HUANG <mincong.h@gmail.com>
2018-03-01 07:32:16 +0100

    Immutable set in Java 9

---
+++ java9/src/test/java/io/mincongh/java9/api/TestImmutableSet.java
@@ -0,0 +1,48 @@
+package io.mincongh.java9.api;
+
+import java.util.List;
+import java.util.Set;
+import java.util.function.Consumer;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+/**
+ * Tests immutable set.
+ *
+ * @author Mincong Huang
+ */
+public class TestImmutableSet {
+
+  @Test
+  public void createImmutableSet() {
+    Set<String> words = Set.of("Hello", "Java9");
+    assertTrue(words.contains("Hello"));
+    assertTrue(words.contains("Java9"));
+  }
+
+  @Test
+  public void immutability() {
+    Set<String> words = Set.of("Hello", "Java9");
+    // Some of the unsupported methods
+    List<Consumer<String>> consumers =
+        List.of(
+            words::add,
+            words::remove,
+            s -> words.addAll(List.of(s)),
+            s -> words.removeAll(List.of(s)),
+            s -> words.retainAll(List.of(s)));
+
+    consumers.forEach(this::assertThrown);
+  }
+
+  private void assertThrown(Consumer<String> consumer) {
+    try {
+      consumer.accept("Hello");
+      fail("The operation should not be accepted.");
+    } catch (UnsupportedOperationException e) {
+      // Ok
+    }
+  }
+}
---
+++ java9/src/test/java/io/mincongh/java9/api/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * New APIs created in Java 9.
+ *
+ * @author Mincong Huang
+ */
+package io.mincongh.java9.api;
