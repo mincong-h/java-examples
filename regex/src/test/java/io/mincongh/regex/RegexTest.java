34a571ca8593dbd612a2b26f923419f9d819df27
Mincong HUANG <mincong.h@gmail.com>
2018-03-21 18:17:54 +0100

    Regex: Add more examples

---
+++ regex/src/test/java/io/mincongh/regex/RegexTest.java
@@ -0,0 +1,27 @@
+package io.mincongh.regex;
+
+import java.util.regex.Pattern;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class RegexTest {
+
+  @Test
+  public void packageName() {
+    String[] pkgNames = {"foo-9.10.json", "bar-10.1.json"};
+    Pattern pattern = Pattern.compile("(foo|bar)-(\\d+\\.\\d+)\\.json");
+    for (String name : pkgNames) {
+      assertTrue(pattern.matcher(name).matches());
+    }
+  }
+
+  @Test
+  public void widgetTypeName() {
+    String[] widgetNames = {"foo-widget-1.0.json", "bar-widget-2.1.json"};
+    Pattern pattern = Pattern.compile("(\\w+)-widget-(\\d+\\.\\d+)\\.json");
+    for (String name : widgetNames) {
+      assertTrue(pattern.matcher(name).matches());
+    }
+  }
+}
