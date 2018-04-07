bcdb8cb6108923ac12bc39cc5cba4d5afeb244be
Mincong HUANG <mincong.h@gmail.com>
2018-04-07 17:01:24 +0200

    Move XML content to test

--- xml/src/test/java/io/mincongh/xml/xstream/util/XStreamUtilTest.java
+++ xml/src/test/java/io/mincongh/xml/xstream/util/XStreamUtilTest.java
@@ -1,42 +1,44 @@
 package io.mincongh.xml.xstream.util;
 
-import static org.assertj.core.api.Java6Assertions.assertThat;
-
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.DomDriver;
 import io.mincongh.xml.xstream.model.Person;
 import io.mincongh.xml.xstream.model.PhoneNumber;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
-/**
- * @author Mincong Huang
- */
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+/** @author Mincong Huang */
 public class XStreamUtilTest {
+
+  private static final String CONTENT_WITH_ALIAS =
+      "<person>\n"
+          + "  <firstName>Foo</firstName>\n"
+          + "  <lastName>Bar</lastName>\n"
+          + "  <age>10</age>\n"
+          + "  <phoneNumber>\n"
+          + "    <code>86</code>\n"
+          + "    <number>123456</number>\n"
+          + "  </phoneNumber>\n"
+          + "</person>";
+
+  private static final String CONTENT_WITHOUT_ALIAS =
+      "<io.mincongh.xml.xstream.model.Person>\n"
+          + "  <firstName>Foo</firstName>\n"
+          + "  <lastName>Bar</lastName>\n"
+          + "  <age>10</age>\n"
+          + "  <phoneNumber>\n"
+          + "    <code>86</code>\n"
+          + "    <number>123456</number>\n"
+          + "  </phoneNumber>\n"
+          + "</io.mincongh.xml.xstream.model.Person>";
 
   private Person p;
 
-  private static String CONTENT_WITH_ALIAS;
-
-  private static String CONTENT_WITHOUT_ALIAS;
-
-  @BeforeClass
-  public static void setUpBeforeClass() throws Exception {
-    CONTENT_WITH_ALIAS = load("person-with-alias.xml");
-    CONTENT_WITHOUT_ALIAS = load("person-without-alias.xml");
-  }
-
-  private static String load(String path) throws IOException {
-    String p = XStreamUtilTest.class.getClassLoader().getResource(path).getPath();
-    return String.join(System.lineSeparator(), Files.readAllLines(Paths.get(p)));
-  }
-
   @Before
-  public void setUp() throws Exception {
+  public void setUp() {
     p = new Person();
     p.setFirstName("Foo");
     p.setLastName("Bar");
@@ -45,23 +47,21 @@ public class XStreamUtilTest {
   }
 
   @Test
-  public void serialize_withAnnotationProcessed() throws Exception {
+  public void serialize_withAnnotationProcessed() {
     String result = XStreamUtil.serialize(p);
     assertThat(result).isEqualTo(CONTENT_WITH_ALIAS);
   }
 
   @Test
-  public void serialize_withoutAnnotationProcessed() throws Exception {
+  public void serialize_withoutAnnotationProcessed() {
     XStream xStream = new XStream(new DomDriver());
     String result = xStream.toXML(p);
     assertThat(result).isEqualTo(CONTENT_WITHOUT_ALIAS);
   }
 
   @Test
-  public void deserialize() throws Exception {
-    String xml = CONTENT_WITH_ALIAS;
-    Person result = XStreamUtil.deserialize(xml, Person.class);
+  public void deserialize() {
+    Person result = XStreamUtil.deserialize(CONTENT_WITH_ALIAS, Person.class);
     assertThat(result).isEqualTo(p);
   }
-
 }
