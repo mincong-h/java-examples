1302b87f72278e4916a603cc76136dc0b80fc11e
Mincong HUANG <mincong.h@gmail.com>
2018-04-26 22:44:49 +0200

    Test branchList

--- jgit/src/test/java/io/mincongh/jgit/GitBranchTest.java
+++ jgit/src/test/java/io/mincongh/jgit/GitBranchTest.java
@@ -1,7 +1,9 @@
 package io.mincongh.jgit;
 
+import java.util.List;
 import org.eclipse.jgit.api.errors.RefNotFoundException;
 import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,5 +19,18 @@ public class GitBranchTest extends JGitTest {
   @Test(expected = RefNotFoundException.class)
   public void so50041078() throws Exception {
     git.branchCreate().setStartPoint("origin/topic").setName("topic").call();
+  }
+
+  @Test
+  public void listBranches() throws Exception {
+    git.branchCreate().setName("iss1").call();
+    git.branchCreate().setName("iss2").call();
+
+    List<Ref> refs = git.branchList().call();
+    assertThat(refs).flatExtracting(this::shorten).containsOnly(repo.getBranch(), "iss1", "iss2");
+  }
+
+  private String shorten(Ref ref) {
+    return Repository.shortenRefName(ref.getName());
   }
 }
