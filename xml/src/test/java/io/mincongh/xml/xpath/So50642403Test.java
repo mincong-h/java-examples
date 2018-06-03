409eee2d740313316cf7cb89a9a54895262dcf2e
Mincong HUANG <mincong.h@gmail.com>
2018-06-03 07:42:49 +0200

    SO-50642403: XPATH: Insert xml nodes into another XML document
    https://stackoverflow.com/questions/50642403/xpath-insert-xml-nodes-into-another-xml-document/

---
+++ xml/src/test/java/io/mincongh/xml/xpath/So50642403Test.java
@@ -0,0 +1,82 @@
+package io.mincongh.xml.xpath;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathFactory;
+import org.junit.Before;
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+
+import static javax.xml.xpath.XPathConstants.NODE;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Stack Overflow: XPATH: Insert xml nodes into another XML document
+ *
+ * @author Mincong Huang
+ */
+public class So50642403Test {
+
+  private static final String XML1 =
+      "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+          + "<letterContent>\n"
+          + "    <key1>key1</key1>\n"
+          + "    <key2>key2</key2>\n"
+          + "    <type>456</type>\n"
+          + "    <object1>789</object1>\n"
+          + "    <effectiveDate>00</effectiveDate>\n"
+          + "    <expandedData attr1=\"case1\">\n"
+          + "            <expandedData attr2=\"value2\">\n"
+          + "                <data attrD=\"VD2\">value D2</data>\n"
+          + "            </expandedData>\n"
+          + "            <expandedData attr3=\"value3\">\n"
+          + "                <data attrD=\"vd3\">value D3</data>\n"
+          + "            </expandedData>\n"
+          + "    </expandedData>\n"
+          + "</letterContent>";
+  private static final String XML2 =
+      "<expandedData attr4=\"value4\">\n"
+          + "    <data attrd4=\"vd4\">value d4</data>\n"
+          + "    <name nameattr=\"specific_name\"/>\n"
+          + "</expandedData>\n";
+
+  private Document doc1;
+  private Document doc2;
+
+  @Before
+  public void setUp() throws Exception {
+    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    try (InputStream in = new ByteArrayInputStream(XML1.getBytes(StandardCharsets.UTF_8))) {
+      doc1 = factory.newDocumentBuilder().parse(in);
+    }
+    try (InputStream in = new ByteArrayInputStream(XML2.getBytes(StandardCharsets.UTF_8))) {
+      doc2 = factory.newDocumentBuilder().parse(in);
+    }
+  }
+
+  @Test
+  public void name() throws Exception {
+    XPath xpath = XPathFactory.newInstance().newXPath();
+    Node e1 = (Node) xpath.evaluate("//expandedData[@attr1='case1']", doc1, NODE);
+    Node e2 = (Node) xpath.evaluate("//expandedData[@attr4='value4']", doc2, NODE);
+    e1.appendChild(doc1.importNode(e2, true));
+
+    StringWriter writer = new StringWriter();
+    Transformer transformer = TransformerFactory.newInstance().newTransformer();
+    transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+    transformer.transform(new DOMSource(doc1), new StreamResult(writer));
+    String content = writer.toString();
+    assertThat(content).contains("<data attrd4=\"vd4\">value d4</data>");
+    assertThat(content).contains("<name nameattr=\"specific_name\"/>");
+  }
+}
