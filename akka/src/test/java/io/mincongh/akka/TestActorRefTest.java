40b0b36dd584e10d62e2500eb0edaae07b918def
Mincong HUANG <mincong.h@gmail.com>
2020-01-08 20:48:13 +0100

    Akka: typo

--- akka/src/test/java/io/mincongh/akka/TestActorRefTest.java
+++ akka/src/test/java/io/mincongh/akka/TestActorRefTest.java
@@ -19,7 +19,7 @@ import static org.junit.Assert.assertEquals;
  * friendly.
  *
  * @author Mincong Huang
- * @see <a href="https://doc.akka.io/docs/akka/current/testing.html>Testing Classic Actors</a>
+ * @see <a href="https://doc.akka.io/docs/akka/current/testing.html">Testing Classic Actors</a>
  */
 public class TestActorRefTest {
 
