04e19d5c0532bd65ea4171c415d00d5b6a8c8e43
Mincong Huang <mincong-h@users.noreply.github.com>
2020-03-17 22:50:55 +0100

    Concurrency: More examples about CompletableFuture (#21)
    * Concurrency: Simplify code using Runnable
    
    * Concurrency: Test thenAcceptBoth
    
    * Concurrency: Add link to CompletionStage
    
    * Concurrency: Test exceptionally
    
    * Concurrency: Test factory methods
    
    * Concurrency: Test complete APIs
    
    * Concurrency: Test whenComplete

--- concurrency/src/test/java/io/mincong/concurrency/CompletableFutureTest.java
+++ concurrency/src/test/java/io/mincong/concurrency/CompletableFutureTest.java
@@ -1,8 +1,8 @@
 package io.mincong.concurrency;
 
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
-import java.util.function.Supplier;
 import org.junit.Test;
 
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
@@ -13,21 +13,124 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 /**
  * @author Mincong Huang
  * @see java.util.concurrent.CompletableFuture
+ * @see java.util.concurrent.CompletionStage
  */
 public class CompletableFutureTest {
 
+  /*
+   * ----- Completed API -----
+   *
+   * Factory methods used for creating a completed completable future
+   * or stage:
+   *   - Success: completed{Future,Stage}
+   *   - Failure: failed{Future,Stage}
+   */
+
   @Test
-  public void create_completedFuture() throws Exception {
+  public void completedFuture() throws Exception {
     var future = CompletableFuture.completedFuture("Hello, Java");
+    assertThat(future.isDone()).isTrue();
+    assertThat(future.get()).isEqualTo("Hello, Java");
+    assertThat(future.isCompletedExceptionally()).isFalse();
+  }
+
+  @Test
+  public void completedStage() throws Exception {
+    var future = CompletableFuture.completedStage("Hello, Java").toCompletableFuture();
+    assertThat(future.isDone()).isTrue();
+    assertThat(future.get()).isEqualTo("Hello, Java");
+    assertThat(future.isCompletedExceptionally()).isFalse();
+  }
+
+  @Test
+  public void failedFuture() {
+    var future = CompletableFuture.failedFuture(new IllegalArgumentException());
+    assertThat(future.isDone()).isTrue();
+    assertThatThrownBy(future::get).isInstanceOf(ExecutionException.class);
+    assertThat(future.isCompletedExceptionally()).isTrue();
+  }
+
+  @Test
+  public void failedStage() {
+    var future =
+        CompletableFuture.failedStage(new IllegalArgumentException()).toCompletableFuture();
+    assertThat(future.isDone()).isTrue();
+    assertThatThrownBy(future::get).isInstanceOf(ExecutionException.class);
+    assertThat(future.isCompletedExceptionally()).isTrue();
+  }
+
+  /* ----- Complete APIs ----- */
+
+  @Test
+  public void complete() throws Exception {
+    var latch = new CountDownLatch(1);
+    var future = new CompletableFuture<String>();
+
+    submit(
+        () -> {
+          future.complete("Hello, Java");
+          latch.countDown();
+        });
+
+    latch.await(1, SECONDS);
     assertThat(future.get()).isEqualTo("Hello, Java");
   }
 
   @Test
-  public void create_newFuture() throws Exception {
+  public void completeExceptionally() throws Exception {
+    var latch = new CountDownLatch(1);
     var future = new CompletableFuture<String>();
-    submit(() -> future.complete("Hello, Java"));
+
+    submit(
+        () -> {
+          future.completeExceptionally(new IllegalArgumentException());
+          latch.countDown();
+        });
+
+    latch.await(1, SECONDS);
+    assertThat(future.isCompletedExceptionally()).isTrue();
+    assertThatThrownBy(future::get).isInstanceOf(ExecutionException.class);
+  }
+
+  @Test
+  public void completeAsync() throws Exception {
+    var latch = new CountDownLatch(1);
+    var future = new CompletableFuture<String>();
+
+    submit(
+        () ->
+            future.completeAsync(
+                () -> {
+                  latch.countDown();
+                  return "Hello, Java";
+                }));
+
+    latch.await(1, SECONDS);
     assertThat(future.get()).isEqualTo("Hello, Java");
   }
+
+  /* ----- exceptionally API ----- */
+
+  @Test
+  public void exceptionally() throws Exception {
+    var latch = new CountDownLatch(1);
+    var future1 = new CompletableFuture<String>();
+    var future2 = future1.exceptionally(ex -> "Enough Java for today");
+
+    submit(
+        () -> {
+          future1.completeExceptionally(new IllegalArgumentException());
+          latch.countDown();
+        });
+
+    latch.await(1, SECONDS);
+    assertThat(future1.isCompletedExceptionally()).isTrue();
+    assertThatThrownBy(future1::get).isInstanceOf(ExecutionException.class);
+    assertThat(future2.isCompletedExceptionally()).isFalse();
+    assertThat(future2.get()).isEqualTo("Enough Java for today");
+  }
+
+  /* ----- get API ----- */
 
   @Test
   public void get_successful() throws Exception {
@@ -41,6 +144,8 @@ public class CompletableFutureTest {
     var future = new CompletableFuture<String>();
     assertThatThrownBy(() -> future.get(1, MILLISECONDS)).isInstanceOf(TimeoutException.class);
   }
+
+  /* ----- then{Action} APIs ----- */
 
   @Test
   public void thenAccept() throws Exception {
@@ -57,6 +162,33 @@ public class CompletableFutureTest {
 
     latch.await(1, SECONDS);
     assertThat(future.get()).isEqualTo("Java");
+    assertThat(sentence.get()).isEqualTo("Hello, Java");
+  }
+
+  @Test
+  public void thenAcceptBoth() throws Exception {
+    var latch = new CountDownLatch(1);
+    var sentence = new AtomicReference<String>();
+    var future1 = new CompletableFuture<String>();
+    var future2 = new CompletableFuture<String>();
+    var future3 =
+        future1.thenAcceptBoth(
+            future2,
+            (prefix, language) -> {
+              sentence.set(prefix + ", " + language);
+              latch.countDown();
+            });
+
+    submit(
+        () -> {
+          future1.complete("Hello");
+          future2.complete("Java");
+        });
+
+    latch.await(1, SECONDS);
+    assertThat(future1.get()).isEqualTo("Hello");
+    assertThat(future2.get()).isEqualTo("Java");
+    assertThat(future3.get()).isNull();
     assertThat(sentence.get()).isEqualTo("Hello, Java");
   }
 
@@ -92,7 +224,6 @@ public class CompletableFutureTest {
         () -> {
           future1.complete("Hello");
           future2.complete("Java");
-          return null;
         });
 
     assertThat(future1.get()).isEqualTo("Hello");
@@ -100,12 +231,53 @@ public class CompletableFutureTest {
     assertThat(future3.get()).isEqualTo("Hello, Java");
   }
 
-  private static void submit(Supplier<?> supplier) {
+  @Test
+  public void whenComplete() throws Exception {
+    var latch = new CountDownLatch(1);
+    var success1 = new AtomicInteger(0);
+    var failure1 = new AtomicInteger(0);
+    var success2 = new AtomicInteger(0);
+    var failure2 = new AtomicInteger(0);
+    var future1 = new CompletableFuture<String>();
+    var future2 = new CompletableFuture<String>();
+    future1.whenComplete(
+        (s, ex) -> {
+          if (ex == null) {
+            success1.incrementAndGet();
+          } else {
+            failure1.incrementAndGet();
+          }
+        });
+    future2.whenComplete(
+        (s, ex) -> {
+          if (ex == null) {
+            success2.incrementAndGet();
+          } else {
+            failure2.incrementAndGet();
+          }
+        });
+    submit(
+        () -> {
+          future1.complete("Hello, Java");
+          future2.completeExceptionally(new IllegalArgumentException());
+          latch.countDown();
+        });
+
+    latch.await(1, SECONDS);
+    assertThat(success1.get()).isEqualTo(1);
+    assertThat(success2.get()).isEqualTo(0);
+    assertThat(failure1.get()).isEqualTo(0);
+    assertThat(failure2.get()).isEqualTo(1);
+  }
+
+  /* ----- Utility ----- */
+
+  private static void submit(Runnable runnable) {
     Executors.newCachedThreadPool()
         .submit(
             () -> {
               Thread.sleep(100);
-              supplier.get();
+              runnable.run();
               return null;
             });
   }
