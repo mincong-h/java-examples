8a9339094c8e155cc1312325798140ede873f38a
Mincong HUANG <mincong.h@gmail.com>
2020-04-12 17:55:08 +0200

    Add Jackson example

---
+++ immutables/src/test/java/io/mincong/immutables/jackson/AddressTest.java
@@ -0,0 +1,38 @@
+package io.mincong.immutables.jackson;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AddressTest {
+
+  private final ObjectMapper mapper = new ObjectMapper();
+  private final String content =
+      "{\"address\":\"55 Rue du Faubourg Saint-Honoré\",\"city\":\"Paris\",\"zipcode\":\"75008\"}";
+
+  @Test
+  public void itCanSerialize() throws Exception {
+    var elysee =
+        ImmutableAddress.builder()
+            .address("55 Rue du Faubourg Saint-Honoré")
+            .city("Paris")
+            .postalCode("75008")
+            .build();
+
+    var json = mapper.writeValueAsString(elysee);
+    assertThat(json).isEqualTo(content);
+  }
+
+  @Test
+  public void itCanDeserialize() throws Exception {
+    var actual = mapper.readValue(content, ImmutableAddress.class);
+        var expected = ImmutableAddress.builder()
+            .address("55 Rue du Faubourg Saint-Honoré")
+            .city("Paris")
+            .postalCode("75008")
+            .build();
+
+    assertThat(actual).isEqualTo(expected);
+  }
+}
