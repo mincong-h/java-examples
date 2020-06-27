b0951d8aa2ed698bed6b5bae79a8fa0d2b9743bd
Mincong HUANG <mincong.h@gmail.com>
2020-06-27 18:48:52 +0200

    wip

--- concurrency/src/test/java/io/mincong/concurrency/BlogCompletableFutureMotivationTest.java
+++ concurrency/src/test/java/io/mincong/concurrency/BlogCompletableFutureMotivationTest.java
@@ -3,8 +3,9 @@ package io.mincong.concurrency;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.HashSet;
-import java.util.concurrent.Callable;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import org.junit.jupiter.api.Test;
@@ -19,7 +20,7 @@ class BlogCompletableFutureMotivationTest {
   void threadPoolMultiTasks() throws Exception {
     var tasks =
         IntStream.range(0, 5)
-            .mapToObj(i -> (Callable<Integer>) () -> i)
+            .mapToObj(i -> (Callable<Integer>) () -> i + 1)
             .collect(Collectors.toList());
 
     var results = new HashSet<Integer>();
@@ -27,7 +28,7 @@ class BlogCompletableFutureMotivationTest {
     try {
       var futures = executor.invokeAll(tasks);
       for (var f : futures) {
-        var result = f.get();
+        int result = f.get();
         // perform callback
         results.add(result);
       }
@@ -36,5 +37,60 @@ class BlogCompletableFutureMotivationTest {
     }
 
     assertThat(results).containsExactly(1, 2, 3, 4, 5);
+  }
+
+  @Test
+  void completableFutureMultiTasks() throws Exception {
+    var results = new ConcurrentSkipListSet<>();
+    var tasks =
+        IntStream.range(0, 5)
+            .mapToObj(
+                i ->
+                    CompletableFuture.completedFuture(i)
+                        // perform callback
+                        .thenApply(v -> v + 1)
+                        .thenAccept(results::add))
+            .toArray(CompletableFuture[]::new);
+
+    var cf = CompletableFuture.allOf(tasks);
+    cf.get();
+
+    assertThat(results).containsExactly(1, 2, 3, 4, 5);
+  }
+
+  @Test
+  void anotherExecutor() throws Exception {
+    var executor = Executors.newSingleThreadExecutor();
+    /*
+     * Provide your executor to isolate long-running operation.
+     */
+    var cf = CompletableFuture.supplyAsync(this::getSthSlow, executor);
+    assertThat(cf.get()).isEqualTo(1);
+  }
+
+  private int getSthSlow() {
+    return 1;
+  }
+
+  @Test
+  void apply() throws Exception {
+    // function
+    Function<Integer, String> fn = String::valueOf;
+    var s1 = fn.apply(1);
+    assertThat(s1).isEqualTo("1");
+
+    // CF
+    var s2 = CompletableFuture.completedFuture(1).thenApply(String::valueOf).get();
+    assertThat(s2).isEqualTo("1");
+  }
+
+  @Test
+  void accept() throws Exception {
+    // consumer
+    Consumer<Integer> consumer = v -> assertThat(v).isEqualTo(1);
+    consumer.accept(1);
+
+    // CF
+    CompletableFuture.completedFuture(1).thenAccept(consumer).get();
   }
 }
