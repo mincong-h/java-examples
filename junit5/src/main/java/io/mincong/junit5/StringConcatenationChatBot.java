18ae320e4debbc80705d715df6a7528059c05b4d
Mincong HUANG <mincong.h@gmail.com>
2021-01-31 16:05:45 +0100

    Add a untested method

--- junit5/src/main/java/io/mincong/junit5/StringConcatenationChatBot.java
+++ junit5/src/main/java/io/mincong/junit5/StringConcatenationChatBot.java
@@ -6,4 +6,14 @@ public class StringConcatenationChatBot implements ChatBot {
   public String sayHello(String username) {
     return "Hello, " + username;
   }
+
+  /**
+   * In IntelliJ IDEA, run ChatBotTest with coverage and observe the test coverage here. You can see
+   * that this test is not tested. One advantage of using parameterized testing is that it can
+   * increase the test coverage easily with difference scenarios.
+   */
+  @SuppressWarnings("unused")
+  public String sayNo(String username) {
+    return "No, " + username;
+  }
 }
