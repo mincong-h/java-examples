621cc0b306de9f712bd427bda5a9bb0b6a0c4364
Mincong HUANG <mincong.h@gmail.com>
2021-02-06 08:01:50 +0100

    JUnit: Create demo for enum conversion

---
+++ junit5/src/main/java/io/mincong/junit5/FilePermission.java
@@ -0,0 +1,33 @@
+package io.mincong.junit5;
+
+public enum FilePermission {
+  X(1),
+  W(2),
+  WX(3),
+  R(4),
+  RX(5),
+  RW(6),
+  RWX(7);
+
+  private final boolean readable;
+  private final boolean writable;
+  private final boolean executable;
+
+  FilePermission(int v) {
+    this.executable = (v & 0b001) == 1;
+    this.writable = (v & 0b010) == 2;
+    this.readable = (v & 0b100) == 4;
+  }
+
+  boolean isReadable() {
+    return readable;
+  }
+
+  boolean isWritable() {
+    return writable;
+  }
+
+  boolean isExecutable() {
+    return executable;
+  }
+}
---
+++ junit5/src/test/java/io/mincong/junit5/FilePermissionTest.java
@@ -0,0 +1,50 @@
+package io.mincong.junit5;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class FilePermissionTest {
+  @ParameterizedTest
+  @CsvSource({
+    "R, false",
+    "W, false",
+    "RW, false",
+    "X, true",
+    "RX, true",
+    "WX, true",
+    "RWX, true",
+  })
+  void isExecutable(FilePermission perm, boolean isExecutable) {
+    assertThat(perm.isExecutable()).isEqualTo(isExecutable);
+  }
+
+  @ParameterizedTest
+  @CsvSource({
+    "W, false",
+    "X, false",
+    "WX, false",
+    "R, true",
+    "RW, true",
+    "RX, true",
+    "RWX, true",
+  })
+  void isReadable(FilePermission perm, boolean isReadable) {
+    assertThat(perm.isReadable()).isEqualTo(isReadable);
+  }
+
+  @ParameterizedTest
+  @CsvSource({
+    "X, false",
+    "R, false",
+    "RX, false",
+    "W, true",
+    "WX, true",
+    "RW, true",
+    "RWX, true",
+  })
+  void isWritable(FilePermission perm, boolean isWritable) {
+    assertThat(perm.isWritable()).isEqualTo(isWritable);
+  }
+}
