773b36f5751ed627c283388b0a776459908b56f1
Mincong HUANG <mincong.h@gmail.com>
2021-02-21 16:22:13 +0100

    Flatten the project structure

--- mongo/src/test/java/io/mincongh/mongodb/AbstractMongoIT.java
+++ mongo/src/test/java/io/mincongh/mongodb/AbstractMongoIT.java
@@ -1,4 +1,4 @@
-package io.mincongh.mongodb.legacy;
+package io.mincongh.mongodb;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
--- mongo/src/test/java/io/mincongh/mongodb/MongoCollectionIT.java
+++ mongo/src/test/java/io/mincongh/mongodb/MongoCollectionIT.java
@@ -1,4 +1,4 @@
-package io.mincongh.mongodb.legacy;
+package io.mincongh.mongodb;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
