3f00e77567f0b74886039ec8610a6d843d7ecf56
Mincong HUANG <mincong.h@gmail.com>
2021-06-18 18:11:35 +0200

    Refactor PersonService

--- spring-mongo/src/main/java/io/mincong/spring/mongodb/PersonService.java
+++ spring-mongo/src/main/java/io/mincong/spring/mongodb/PersonService.java
@@ -2,46 +2,19 @@ package io.mincong.spring.mongodb;
 
 import static org.springframework.data.mongodb.core.query.Criteria.where;
 import static org.springframework.data.mongodb.core.query.Query.query;
-import static org.springframework.data.mongodb.core.query.Update.update;
 
-import com.mongodb.client.MongoClients;
-import java.util.List;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoOperations;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PersonService {
 
-  private static final Logger log = LoggerFactory.getLogger(PersonService.class);
+  @Autowired MongoOperations operations;
 
-  public static void main(String[] args) {
-
-    MongoOperations mongoOps =
-        new MongoTemplate(new SimpleMongoClientDatabaseFactory(MongoClients.create(), "database"));
-    Person p = new Person("Joe", 34);
-
-    // Insert is used to initially store the object into the database.
-    mongoOps.insert(p);
-    log.info("Insert: " + p);
-
-    // Find
-    p = mongoOps.findById(p.getId(), Person.class);
-    log.info("Found: " + p);
-
-    // Update
-    mongoOps.updateFirst(query(where("name").is("Joe")), update("age", 35), Person.class);
-    p = mongoOps.findOne(query(where("name").is("Joe")), Person.class);
-    log.info("Updated: " + p);
-
-    // Delete
-    mongoOps.remove(p);
-
-    // Check that deletion worked
-    List<Person> people = mongoOps.findAll(Person.class);
-    log.info("Number of people = : " + people.size());
-
-    mongoOps.dropCollection(Person.class);
+  public Optional<Person> findPerson(String name) {
+    var p = operations.findOne(query(where("name").is(name)), Person.class);
+    return Optional.ofNullable(p);
   }
 }
