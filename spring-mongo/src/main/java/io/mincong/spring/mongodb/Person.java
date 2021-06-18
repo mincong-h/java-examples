82b10327ef30bcd457f7937c8d6a82717c8886b7
Mincong HUANG <mincong.h@gmail.com>
2021-06-18 18:38:16 +0200

    Add one test

--- spring-mongo/src/main/java/io/mincong/spring/mongodb/Person.java
+++ spring-mongo/src/main/java/io/mincong/spring/mongodb/Person.java
@@ -7,7 +7,15 @@ public class Person {
   private String name;
   private int age;
 
+  public Person() {} // for spring
+
   public Person(String name, int age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  public Person(String id, String name, int age) {
+    this.id = id;
     this.name = name;
     this.age = age;
   }
