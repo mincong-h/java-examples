fc1d6f125c0dc3fc976fe300af8fd1cc0ce9de6f
Mincong HUANG <mincong.h@gmail.com>
2021-06-18 15:35:07 +0200

    Use logback

--- spring-mongo/src/main/java/io/mincong/spring/mongodb/MongoApp.java
+++ spring-mongo/src/main/java/io/mincong/spring/mongodb/MongoApp.java
@@ -3,22 +3,22 @@ package io.mincong.spring.mongodb;
 import static org.springframework.data.mongodb.core.query.Criteria.where;
 
 import com.mongodb.client.MongoClients;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
 
 public class MongoApp {
 
-  private static final Log log = LogFactory.getLog(MongoApp.class);
+  private static final Logger log = LoggerFactory.getLogger(MongoApp.class);
 
   public static void main(String[] args) {
 
     MongoOperations mongoOps = new MongoTemplate(MongoClients.create(), "database");
     mongoOps.insert(new Person("Joe", 34));
 
-    log.info(mongoOps.findOne(new Query(where("name").is("Joe")), Person.class));
+    log.info("{}", mongoOps.findOne(new Query(where("name").is("Joe")), Person.class));
 
     mongoOps.dropCollection("person");
   }
