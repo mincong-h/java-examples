e4642e03b560871678d9e338d753f79173d2ac82
Mincong HUANG <mincong.h@gmail.com>
2021-06-18 16:00:08 +0200

    Add more operations in the demo

---
+++ spring-mongo/src/main/java/io/mincong/spring/mongodb/AppConfig.java
@@ -0,0 +1,17 @@
+package io.mincong.spring.mongodb;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+  /*
+   * Use the standard Mongo driver API to create a com.mongodb.client.MongoClient instance.
+   */
+  public @Bean MongoClient mongoClient() {
+    return MongoClients.create("mongodb://localhost:27017");
+  }
+}
