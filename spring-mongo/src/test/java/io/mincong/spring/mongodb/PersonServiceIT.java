b4e1275a3b372b5d9e6fdd695c68005cc0a6da86
Mincong HUANG <mincong.h@gmail.com>
2021-06-18 17:12:42 +0200

    Add test

--- spring-mongo/src/test/java/io/mincong/spring/mongodb/PersonServiceIT.java
+++ spring-mongo/src/test/java/io/mincong/spring/mongodb/PersonServiceIT.java
@@ -1,3 +1,33 @@
 package io.mincong.spring.mongodb;
 
-class PersonServiceIT {}
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+import static org.springframework.data.mongodb.core.query.Query.query;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.data.mongodb.core.MongoOperations;
+
+@DataMongoTest
+class PersonServiceIT {
+  @Autowired MongoOperations operations;
+
+  Person sansa, arya;
+
+  @BeforeEach
+  void setUp() {
+    this.sansa = new Person("Sansa Stark", 20);
+    this.arya = new Person("Arya Stark", 20);
+
+    operations.save(sansa);
+    operations.save(arya);
+  }
+
+  @Test
+  void queryId() {
+    var query = query(where("name").is("Sansa Stark"));
+    assertThat(operations.find(query, Person.class)).contains(sansa);
+  }
+}
