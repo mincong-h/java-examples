ead04f198caf59895400d8402181ff239977c698
Mincong HUANG <mincong.h@gmail.com>
2021-06-26 21:25:07 +0200

    Fix tests

--- spring-mongo/src/main/java/io/mincong/spring/mongodb/AppConfig.java
+++ spring-mongo/src/main/java/io/mincong/spring/mongodb/AppConfig.java
@@ -2,8 +2,10 @@ package io.mincong.spring.mongodb;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 
+@SpringBootApplication
 public class AppConfig extends AbstractMongoClientConfiguration {
 
   @Override
